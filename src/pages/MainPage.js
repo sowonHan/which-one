244a86d05f723355adc3b3647ca1a35bc88c0fe8
sowonHan <rippang781@gmail.com>
2022-12-12 21:55:38 +0900

    2022.12.12

---
+++ src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <>
+      <div>헤더</div>
+      <div>모든 영화와 TV 프로그램을 한 번에</div>
+      <div>실시간으로 인기 작품을 확인하고</div>
+      <div>나에게 맞는 OTT 플랫폼을 찾으세요.</div>
+      <div>푸터</div>
+    </>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/MoviePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MoviePage = () => {
+  return (
+    <>
+      <div>헤더</div>
+      <div>
+        주간 박스오피스
+        <div>왼쪽에 hover 시 영화 정보 간략하게 뜨기</div>
+        <div>오른쪽에 박스오피스 목록</div>
+      </div>
+      <div>OTT 포함한 인기 영화</div>
+      <div>OTT 포함한 최신 영화</div>
+      <div>푸터</div>
+    </>
+  );
+};
+
+export default MoviePage;
