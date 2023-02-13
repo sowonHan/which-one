61f413541a3086cc52eed30e8128994d39d563bb
sowonHan <rippang781@gmail.com>
2023-02-13 20:26:31 +0900

    2023.02.13 트렌드 1위 섹션 scss와 레이아웃 수정

--- src/components/HotTv.js
+++ src/components/HotTv.js
@@ -1,35 +1,47 @@
 import React from "react";
 import ReactPlayer from "react-player/youtube";
 import "../styles/HotTv.scss";
+import { imageTMDB } from "../lib/config";
 
 const HotTv = ({ trailer, program, loadingTrailer }) => {
   return (
     <>
       <section>
-        <div className="player-wrapper">
+        <div className="hot-container">
           {loadingTrailer ? (
             <div className="player-loading" />
           ) : (
             trailer && (
-              <ReactPlayer
-                className="player"
-                url={`https://www.youtube.com/embed/${trailer.key}?enablejsapi=1&origin=http://localhost:3000`}
-                playing={true}
-                loop={true}
-                muted={true}
-                width="100%"
-                height="100%"
-              />
+              <div className="player-wrapper">
+                <ReactPlayer
+                  className="react-player"
+                  url={`https://www.youtube.com/embed/${trailer.key}?enablejsapi=1&origin=http://localhost:3000`}
+                  playing={false}
+                  loop={true}
+                  muted={true}
+                  width="100%"
+                  height="150%"
+                />
+              </div>
             )
           )}
-          <div className="text-container">
+          <div className="info-container">
             {loadingTrailer ? (
               <div></div>
             ) : (
               program && (
-                <div>
-                  <h1 className="en-name">{program.name}</h1>
-                  <h6 className="origin-name">{program.original_name}</h6>
+                <div className="info-wrapper">
+                  <div className="poster-container">
+                    <img
+                      className="poster"
+                      src={`${imageTMDB}/original${program.poster_path}`}
+                      alt={`${program.name}의 포스터`}
+                    />
+                  </div>
+                  <div className="text-container">
+                    <h1 className="en-name">{program.name}</h1>
+                    <h6 className="origin-name">{program.original_name}</h6>
+                  </div>
                 </div>
               )
             )}
