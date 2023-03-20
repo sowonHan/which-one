afb486afec49eb13272dfb1f21b03c742ceef0d2
sowonHan <rippang781@gmail.com>
2023-03-20 22:22:34 +0900

    2023.03.20 홈화면 html,css 진행 중

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,42 @@
 import React from "react";
-import logo from "../../public/logo192px.png";
+import logo from "../images/headerLogo.png";
 import { BiLinkExternal } from "react-icons/bi";
 import "../styles/Footer.scss";
 
 const Footer = () => {
   return (
     <>
-      <div className="all-container">
-        <div className="logo-con">
+      <div className="footer-container">
+        <div className="info-row">
+          <div>
+            <h6>Used Skills</h6>
+            <p>HTML5</p>
+            <p>Sass</p>
+            <p>Javascript</p>
+            <p>React</p>
+            <p>Redux</p>
+            <p>Redux-thunk</p>
+          </div>
+          <div>
+            <h6>API</h6>
+            <div>The Movie DB</div>
+            <div>영화진흥위원회 Open API</div>
+          </div>
+          <div>
+            <h6>Image Source</h6>
+            <p>Unsplash</p>
+            <p>Pexels</p>
+          </div>
+        </div>
+        <div className="logo-row">
           <img src={logo} alt="사이트 로고" />
         </div>
-        <div className="info-con">
-          <div className="first-row">
-            <div>
-              <h6>API</h6>
-              <div>The Movie DB</div>
-              <div>영화진흥위원회 Open API</div>
-            </div>
-            <div>
-              <h6>Used Skills</h6>
-              <p>HTML5</p>
-              <p>Sass</p>
-              <p>Javascript</p>
-              <p>React</p>
-              <p>Redux</p>
-              <p>Redux-thunk</p>
-            </div>
-            <div>
-              <h6>Image Source</h6>
-              <p>Unsplash</p>
-              <p>Pexels</p>
-            </div>
-          </div>
-          <div className="second-row">
-            <p>2023 Which-One Demand-On.</p>
-            <a href="https://github.com/sowonHan/which-one">
-              <span>About Project</span>
-              <BiLinkExternal />
-            </a>
-          </div>
+        <div className="bottom-row">
+          <p>2023 Which-One Demand-On.</p>
+          <a href="https://github.com/sowonHan/which-one">
+            <span>About Project</span>
+            <BiLinkExternal />
+          </a>
         </div>
       </div>
     </>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,28 +8,28 @@ const MainPage = () => {
       <section className="first-con">
         <div className="first-text">
           <h2>환영합니다! 어떤 작품을 보여드릴까요?</h2>
-          <p>Which One Demand On(WODO)을 소개합니다</p>
+          <p>Which One Demand On (WODO) 을 소개합니다.</p>
         </div>
       </section>
       <section className="second-con">
         <div className="second-text">
-          <h6>영화관에서 감상하고 싶은 당신께</h6>
-          <p>현재 상영 중인 영화의 박스오피스 순위를 알려드립니다</p>
+          <h3>오늘 영화관이나 갈까? 근데 뭐 보지?</h3>
+          <p>현재 상영 중인 영화의 박스오피스 순위를 알려드립니다.</p>
         </div>
       </section>
       <section className="third-con">
         <div className="third-text">
-          <h6>
-            요즘은 어떤 영화, 드라마, TV 프로그램이 화제를 모으고 있을까요?
-          </h6>
-          <p>둘러보다 보면 관심이 가는 작품을 만날 수 있을 거예요</p>
+          <h3>요즘은 다들 뭐 보고 사나</h3>
+          <p>화제를 모으고 있는 영화, 드라마, TV 프로그램을 소개해드립니다.</p>
+          <p>둘러보다 보면 취향에 맞는 작품을 만날 수 있을 거예요.</p>
         </div>
       </section>
       <section className="fourth-con">
         <div className="fourth-text">
-          <h6>집에서 감상하고 싶다면</h6>
+          <h3>나는 집에서 편안하게 볼래</h3>
           <p>
-            원하는 작품을 어느 OTT 플랫폼에서 시청할 수 있는지는 매우 중요하죠
+            그렇다면 원하는 작품을 어느 OTT 플랫폼에서 시청할 수 있는지가 매우
+            중요하죠.
           </p>
           <p>WODO에서 지금 바로 확인해보세요!</p>
           <p>
@@ -41,8 +41,8 @@ const MainPage = () => {
       </section>
       <section className="last-con">
         <div className="last-text">
-          <h6>모든 영화, 드라마, TV 프로그램 정보를 한 번에</h6>
-          <p>Which One Demand On(WODO)과 함께하세요</p>
+          <h2>모든 영화, 드라마, TV 프로그램 정보를 한 번에</h2>
+          <p>Which One Demand On (WODO) 과 함께하세요.</p>
         </div>
       </section>
       <Footer />
