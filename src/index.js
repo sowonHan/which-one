3aa81044e3690df52b6e2fc8fbc60e7b051bbf5a
sowonHan <rippang781@gmail.com>
2023-04-13 16:21:51 +0900

    2023.04.13 readme 작성 및 호스팅

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/which-one">
         <ScrollToTop />
         <App />
       </BrowserRouter>
